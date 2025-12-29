70fde7debab0faaf8e0fbc6ba7e99b680c3f628b
Matt Lese <matthewjlese@gmail.com>
2025-12-29 10:16:26 -0800

    adds Cli struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    address: String,
+    #[command(flatten)]
+    verbosity: clap_verbosity_flag::Verbosity,
+}
+
 fn main() {
+    let args = Cli::parse();
+
+    env_logger::Builder::new()
+        .filter_level(args.verbosity.into())
+        .init();
+
     println!("Hello, world!");
 }
