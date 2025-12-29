69b9a38fc6dd227c2949439fbd4ae1823220fd48
Matt Lese <matthewjlese@gmail.com>
2025-12-29 11:14:34 -0800

    adds test for empty address arg

---
+++ tests/cli.rs
@@ -0,0 +1,14 @@
+use assert_cmd::cargo::*;
+use predicates::prelude::*;
+
+#[test]
+fn empty_address() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = cargo_bin_cmd!("zurl");
+
+    cmd.arg("");
+    cmd.assert().failure().stderr(predicate::str::contains(
+        "provided address must be a non-empty string",
+    ));
+
+    Ok(())
+}
