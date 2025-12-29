f520f225c2413e89c410b1feb065903c8ad85ec9
Matt Lese <matthewjlese@gmail.com>
2025-12-29 12:49:49 -0800

    adds check for url having port

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ pub fn classify_input(address: &str) -> InputType {
     if let Ok(url) = Url::parse(&with_scheme) {
         // XXX: for now, we're assuming that, if the user didn't input a scheme, we can differentiate between a fuzzy pattern
         //   and a domain that just needs https prepended by the presence of a '.'
-        if url.host_str().map_or(false, |h| h.contains('.')) {
+        if url.host_str().map_or(false, |h| h.contains('.')) || url.port().is_some() {
             return InputType::FullUrl(url);
         }
     }
